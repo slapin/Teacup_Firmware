d3ef8d9555751e4495ac2ce983c0d4542c34b132
Bob Cousins <bobcousins42@googlemail.com>
2012-11-12 00:40:28 +0000

    Add target for sample LPC1114 build

---
+++ target_lpc1114/board.c
@@ -0,0 +1,38 @@
+
+#include "ch.h"
+#include "hal.h"
+
+/**
+ * @brief   PAL setup.
+ * @details Digital I/O ports static configuration as defined in @p board.h.
+ *          This variable is used by the HAL when initializing the PAL driver.
+ */
+#if HAL_USE_PAL || defined(__DOXYGEN__)
+const PALConfig pal_default_config = {
+ {VAL_GPIO0DATA, VAL_GPIO0DIR},
+ {VAL_GPIO1DATA, VAL_GPIO1DIR},
+ {VAL_GPIO2DATA, VAL_GPIO2DIR},
+ {VAL_GPIO3DATA, VAL_GPIO3DIR},
+};
+#endif
+
+/*
+ * Early initialization code.
+ * This initialization must be performed just after stack setup and before
+ * any other initialization.
+ */
+void __early_init(void) {
+
+  lpc111x_clock_init();
+}
+
+/*
+ * Board-specific initialization code.
+ */
+void boardInit(void) {
+
+  /*
+   * Extra, board-specific, initializations.
+   */
+  LPC_IOCON->PIO0_7 = 0xC0;             /* Disables pull-up on LED output. */
+}
---
+++ target_lpc1114/board.h
@@ -0,0 +1,70 @@
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+/*
+ * Setup for a LPC1114 
+ */
+
+/*
+ * Board identifiers.
+ */
+#define BOARD_LPC1114
+#define BOARD_NAME "Opendous MicropendousX-1343 with LPC1114"
+
+/*
+ * Board frequencies.
+ */
+#define SYSOSCCLK               12000000
+
+/*
+ * SCK0 connection on this board.
+ */
+//#define LPC11xx_SPI_SCK0_SELECTOR SCK0_IS_PIO2_11
+
+/*
+ * GPIO 0 initial setup.
+ */
+#define VAL_GPIO0DIR			0x00000000            
+                                
+#define VAL_GPIO0DATA			0x00000000           
+
+/*
+ * GPIO 1 initial setup.
+ */
+#define VAL_GPIO1DIR			0x00000000            
+                                
+#define VAL_GPIO1DATA			0x00000000           
+
+/*
+ * GPIO 2 initial setup.
+ */
+#define VAL_GPIO2DIR            0x00000000
+#define VAL_GPIO2DATA           0x00000000
+
+/*
+ * GPIO 3 initial setup.
+ */
+#define VAL_GPIO3DIR            0x00000000
+#define VAL_GPIO3DATA           0x00000000
+
+/*
+ * Pin definitions.
+ */
+ 
+#define PORT_LED1 	GPIO0
+#define PIN_LED1 	7
+ 
+ 
+
+#if !defined(_FROM_ASM_)
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void boardInit(void);
+#ifdef __cplusplus
+}
+#endif
+#endif /* _FROM_ASM_ */
+
+#endif /* _BOARD_H_ */
